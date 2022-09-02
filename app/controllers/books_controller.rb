f760f5b90dd8928bda9bea75e4bc62dd52dc8140
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-02 11:48:08 +0000

    [Finish]5-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -50,7 +50,7 @@ class BooksController < ApplicationController
 
   def correct_user
     @book = Book.find(params[:id])
-    @user = @book.user_id
+    @user = @book.user
     redirect_to books_path unless @user == current_user
   end
 end
