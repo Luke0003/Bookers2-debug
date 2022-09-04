1e5db4fd737bdc8128a1b65cfd01d6a9b0c1554e
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-04 03:44:41 +0000

    [Finish]5-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,8 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
+    @book_comments = BookComment.all
+    @book_comment = BookComment.new
   end
 
   def index
